d7aa4a34bbfc16afcacf0c23fa84a980b0adffe0
Juan Carlos Oñate <9104613@alumnos.ufv.es>
2025-06-25 17:39:17 +0200

    pdf

--- dis/src/main/java/com/ufv/dis/model/Usuario.java
+++ dis/src/main/java/com/ufv/dis/model/Usuario.java
@@ -1,4 +1,33 @@
 package com.ufv.dis.model;
 
 public class Usuario {
+    private String id;
+    private String nombre;
+    private String apellidos;
+    private String nif;
+    private String email;
+    private Direccion direccion;
+    private MetodoPago metodoPago;
+
+    // Getters y setters
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getApellidos() { return apellidos; }
+    public void setApellidos(String apellidos) { this.apellidos = apellidos; }
+
+    public String getNif() { return nif; }
+    public void setNif(String nif) { this.nif = nif; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public Direccion getDireccion() { return direccion; }
+    public void setDireccion(Direccion direccion) { this.direccion = direccion; }
+
+    public MetodoPago getMetodoPago() { return metodoPago; }
+    public void setMetodoPago(MetodoPago metodoPago) { this.metodoPago = metodoPago; }
 }
--- dis/src/main/java/com/ufv/dis/utils/PdfGenerator.java
+++ dis/src/main/java/com/ufv/dis/utils/PdfGenerator.java
@@ -1,4 +1,52 @@
 package com.ufv.dis.utils;
 
+import com.github.librepdf.openpdf.text.*;
+import com.github.librepdf.openpdf.text.pdf.PdfWriter;
+import com.ufv.dis.model.Usuario;
+
+import java.io.FileOutputStream;
+import java.util.List;
+
 public class PdfGenerator {
+
+    public static void generarPdf(List<Usuario> usuarios) {
+        try {
+            Document document = new Document(PageSize.A4);
+            PdfWriter.getInstance(document, new FileOutputStream("info.pdf"));
+            document.open();
+
+            Font titleFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD, 16);
+            Font normalFont = FontFactory.getFont(FontFactory.HELVETICA, 12);
+
+            document.add(new Paragraph("Listado de Usuarios", titleFont));
+            document.add(new Paragraph(" ")); // espacio
+
+            for (Usuario u : usuarios) {
+                document.add(new Paragraph("ID: " + u.getId(), normalFont));
+                document.add(new Paragraph("Nombre: " + u.getNombre() + " " + u.getApellidos(), normalFont));
+                document.add(new Paragraph("NIF: " + u.getNif(), normalFont));
+                document.add(new Paragraph("Email: " + u.getEmail(), normalFont));
+
+                if (u.getDireccion() != null) {
+                    document.add(new Paragraph("Dirección: " +
+                            u.getDireccion().getCalle() + " " + u.getDireccion().getNumero() + ", " +
+                            u.getDireccion().getPisoLetra() + ", " + u.getDireccion().getCodigoPostal() + ", " +
+                            u.getDireccion().getCiudad(), normalFont));
+                }
+
+                if (u.getMetodoPago() != null) {
+                    document.add(new Paragraph("Método de pago: " +
+                            u.getMetodoPago().getNumeroTarjeta() + " - " +
+                            u.getMetodoPago().getNombreAsociado(), normalFont));
+                }
+
+                document.add(new Paragraph(" "));
+                document.add(new LineSeparator());
+            }
+
+            document.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
